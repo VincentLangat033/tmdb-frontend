c99ca5bb5798fb1aeeed6ea785846a8e667e6857
VincentLangat033 <kimutaiketer033@gmail.com>
2023-04-20 08:53:17 +0300

    Set up react router dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import Login from './components/Authentication/Login';
 import Register from './components/Authentication/Register';
-// import { Route, Routes } from "react-router-dom";
+import Movie from './components/TmDb/Movie'
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
     <Navbar />
-    {/* <Login /> */}
-    <Register />
+    <Routes>
+
+       <Route exact path="/" element ={<Movie />} />
+       <Route exact path="/register" element ={<Register />} />
+       <Route exact path="/login" element ={<Login />} />
+       </Routes>
     </>
 
   );
