194356bb8f1db488b99d5c11a862dedb4d4fdd52
VincentLangat033 <kimutaiketer033@gmail.com>
2023-04-20 15:39:59 +0300

    Login successfully implememted

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -126,9 +126,9 @@ function Login({setUser}) {
 
 
     </form>
-    <h6 className="already-registered"> Dont have an Account? <NavLink to='/register'>
+    <h5 className="already-registered"> Dont have an Account? <NavLink to='/register'>
         <a href='#'> Register  </a>
-        </NavLink></h6>
+        </NavLink></h5>
 
 			
         
