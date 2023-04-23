470b5aa763a4fba36e4a1709b7e739f918c15a92
VincentLangat033 <kimutaiketer033@gmail.com>
2023-04-23 12:25:09 +0300

    Add an array of top genres to select my movies from

--- src/components/TmDb/Movie.js
+++ src/components/TmDb/Movie.js
@@ -10,16 +10,88 @@ function Movie() {
     const DISCOVER_API = MOVIE_API + "discover/movie"
     const API_KEY = "f4726bbe13ac95e896f3571c43f3519f"
     const BACKDROP_PATH = "https://image.tmdb.org/t/p/w1280"
+    // const API_KEY = 'api_key=f4726bbe13ac95e896f3571c43f3519f';
+// const API_KEY = 'api_key=50b389526ca9b840b2cef75d8b8f512e';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const GENRE_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&';
 
     const [playing, setPlaying] = useState(false)
     const [trailer, setTrailer] = useState(null)
     const [movies, setMovies] = useState([])
     const [searchKey, setSearchKey] = useState("")
+    const [genreKey, setGenreKey] = useState("crime")
     const [movie, setMovie] = useState({title: "Loading Movies"})
+    const [selectedGenres, setSelectedGenres] = useState([]);
+
+    let selectedGenre = []
+// An array of sample genres I would like to display
+const genres = [
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+  
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+
+
+const renderTags = ()=>(
+    genres.map(genre=>(
+        <div className="tag">{genre.name}</div>
+    ))
+)
 
     useEffect(() => {
         fetchMovies()
-    }, [])
+        fetchMoviesByGenres();
+    }, [selectedGenres])
 
     const fetchMovies = async (event) => {
         if (event) {
@@ -41,6 +113,36 @@ function Movie() {
             await fetchMovie(data.results[0].id)
         }
     }
+
+    const fetchMoviesByGenres = async (event) => {
+        if (event) {
+            event.preventDefault()
+        }
+
+        if (selectedGenres.length === 0) {
+          return;
+        }
+        const genreIds = selectedGenres.map(genre => genre.id);
+        const url = `${MOVIE_API}/discover/movie?api_key=${API_KEY}&with_genres=${genreIds.join(',')}`;
+        const response = await axios.get(url);
+        setMovies(response.data.results);
+        setMovie(response.data.results[0])
+        if (response.data.results.length) {
+            await fetchMovie(response.data.results[0].id)
+        }
+      }
+      const handleGenreClick = (genre) => {
+        const index = selectedGenres.findIndex(selectedGenre => selectedGenre.id === genre.id);
+        if (index !== -1) {
+          const updatedGenres = [...selectedGenres];
+          updatedGenres.splice(index, 1);
+          setSelectedGenres(updatedGenres);
+        } else {
+          setSelectedGenres([...selectedGenres, genre]);
+        }
+      };
+
+   
 
     const fetchMovie = async (id) => {
         const {data} = await axios.get(`${MOVIE_API}movie/${id}`, {
@@ -85,7 +187,16 @@ function Movie() {
                            onInput={(event) => setSearchKey(event.target.value)}/>
                     <button className="submit-search" type="submit"><i className="fa fa-search"></i></button>
                 </form>
+                
             </header>
+            {/* <h5>{renderTags()}</h5> */}
+            <div>
+        {genres.map((genre) => (
+          <button className="tag" key={genre.id} onClick={() => handleGenreClick(genre)} style={{backgroundColor: selectedGenres.some(selectedGenre => selectedGenre.id === genre.id) ? 'green' : 'blue'}}>
+            {genre.name}
+          </button>
+        ))}
+      </div>
             {movies.length ?
                 <main>
                     {movie ?
@@ -135,6 +246,7 @@ function Movie() {
 
                     <div className={"center-max-size movie-container"}>
                         {renderMovies()}
+                        {/* console.log(fetchGenres()) */}
                     </div>
                 </main>
                 : 'Sorry, no movies found'}
